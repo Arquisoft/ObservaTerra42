ca51ad9c1fb24dc2e2e703676ca9de431eb445da
joseantonio <joseantonio@Benito.localdomain>
2014-04-28 17:29:37 +0200

    Base de datos antigua

--- jdbc/Main.java
+++ jdbc/Main.java
@@ -7,6 +7,11 @@ public class Main {
 		con.ejecutarQuery("insert into indicator values ('000','alguno','en un lugar de la mancha')");
 		con.cerrarConexion();
 
+		
+		conexion con = new conexion();
+		con.connectToAccess();
+		con.ejecutarQuery("Select * from indicator");
+		con.cerrarConexion();
 	}
 
 }
