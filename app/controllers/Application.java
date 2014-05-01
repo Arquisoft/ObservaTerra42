fcb4dd287045d3b24bc8e32adf426d97127ed35f
DiiSandoval <diisandoval@github.com>
2014-05-01 12:49:01 +0200

    Prueba Vista Login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,21 @@
 package controllers;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import models.*;
-import views.html.*;
-import play.Logger;
-import play.i18n.Messages;
+import models.Business;
+import models.Collaborator;
+import models.Country;
+import models.Indicator;
+import models.Observation;
+import models.User;
+import play.data.Form;
 import play.mvc.Controller;
-import play.mvc.Http.MultipartFormData;
-import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
-import utils.ExcelReader;
-import play.data.*;
+import views.html.bars;
+import views.html.country;
+import views.html.index;
+import views.html.indicator;
+import views.html.login;
+import views.html.observation;
+import views.html.register;
 
 public class Application extends Controller {
 
