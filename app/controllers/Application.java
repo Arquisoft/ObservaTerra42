1283f0696069e6661c7a3f3ff17c0fd3dbb681a3
Sergio González Polán <sergpolan@gmail.com>
2014-05-08 19:42:02 +0200

    Funciona correctamente el subir archivos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,6 @@
 package controllers;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 
 import models.Country;
 import models.Indicator;
@@ -17,6 +15,8 @@ import utils.ThreadWebReader;
 import utils.URLReader;
 import views.html.*;
 import play.*;
+import com.google.common.io.Files;
+
 
 public class Application extends Controller {
 
@@ -31,35 +31,37 @@ public class Application extends Controller {
 		else
 			return ok(country.render(Country.all(), countryForm));
 	}
-	
-	public static Result subirArchivo(){
+
+	public static Result subirArchivo() {
 		return ok(upload.render());
 	}
-	
-	public static Result guardarArchivo() {
-		MultipartFormData body = request().body().asMultipartFormData();
-		FilePart fichero = body.getFile("archivo");
-		
-		String nombreFichero = fichero.getFilename();
-		int indice = nombreFichero.lastIndexOf(".");
-		String type = nombreFichero.substring(indice + 1, nombreFichero.length());
-		
-		if(indice != -1)
-			nombreFichero = nombreFichero.substring(0,indice);
-		
-		String tipo = fichero.getContentType();
 
-		File file = fichero.getFile();
-		int random = new Random().nextInt(100);
-		File newfile = new File("public/data/" + nombreFichero + "."+ type);
+	public static Result guardarArchivo() {
 		try {
-			newfile.createNewFile();
+			MultipartFormData body = request().body().asMultipartFormData();
+			FilePart fichero = body.getFile("archivo");
+
+			String nombreFichero = fichero.getFilename();
+			int indice = nombreFichero.lastIndexOf(".");
+			String type = nombreFichero.substring(indice + 1,
+					nombreFichero.length());
+
+			if (indice != -1)
+				nombreFichero = nombreFichero.substring(0, indice);
+
+			String tipo = fichero.getContentType();
+	
+			File file = fichero.getFile();
+			File newFile = new File("public/data/" + nombreFichero + "." + type);
+			
+			Files.copy(file, newFile);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return index();
-		
+
 	}
 
 	public static Result showCountries() {
@@ -78,22 +80,20 @@ public class Application extends Controller {
 	public static Result bars(String indicator) {
 		return ok(bars.render(Indicator.findByCode(indicator)));
 	}
-	
-	
 
-	public static Result actualizarPaginas(){
+	public static Result actualizarPaginas() {
 		for (int i = 0; i < 3; i++) {
 			ThreadWebReader wb = new ThreadWebReader();
 			new Thread(wb).start();
 		}
-		return index();	
-		}
+		return index();
+	}
 
 	/**/
 	public static Result url() {
 		return ok(url.render(Form.form(URLform.class)));
 	}
-	
+
 	public static Result analizeURL() {
 		Form<URLform> r = Form.form(URLform.class).bindFromRequest();
 		if (r.hasErrors()) {
@@ -101,7 +101,7 @@ public class Application extends Controller {
 		} else {
 			String url = r.get().urlF;
 			URLReader.readerFromWeb(url);
-			//URLReader.google();
+			// URLReader.google();
 			return redirect(routes.Application.index());
 		}
 
@@ -110,7 +110,6 @@ public class Application extends Controller {
 	public static class URLform {
 		public String urlF;
 	}
-
 
 	static Form<Country> countryForm = Form.form(Country.class);
 	static Form<Indicator> indicatorForm = Form.form(Indicator.class);
