5a85de4c0a44fb555a4a6a6bd4b623f6107b9d2a
Sergio González Polán <sergpolan@gmail.com>
2014-05-08 13:05:51 +0200

    Cliente c# : mejorado el menu de interaccion

--- ClienteC#/ClienteObservaterra42/Program.cs
+++ ClienteC#/ClienteObservaterra42/Program.cs
@@ -15,15 +15,79 @@ namespace ClienteAPI
             Console.Write("Seleccione una opción: ");
             switch (Console.Read())
             {
-                case '1': Console.Write("Sub menu paises");
-                // Continuar lógica y extraer métodos //
+                case '1': menuCountry(wb);
+                    break;
+                case '2': menuObservations(wb);
+                    break;
+                case '3': menuIndicators(wb);
+                    break;
+                default:
+                    Console.Write("Opcion incorrecta");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+
+        public static void menuCountry(ClienteWeb cw)
+        {
+            Console.Write("1º) Mostrar todos" + "\n" + 
+                "2º) Mostrar pais " + "\n");
+            Console.Write("Seleccione una opción: ");
+            switch (Console.Read())
+            {
+                case '1': cw.showCountry();
+                    break;
+                case '2':
+                    String pais = Console.ReadLine();
+                    cw.showCountry(pais);
+                    break;
+                default:
+                    Console.WriteLine("Opcion erronea");
                     break;
             }
             Console.ReadKey();
         }
 
-        public static void menuCountry()
+        public static void menuObservations(ClienteWeb cw)
         {
+            Console.Write("1º) Mostrar todos" + "\n" +
+                "2º) Mostrar observacion " + "\n");
+            Console.Write("Seleccione una opción: ");
+            switch (Console.Read())
+            {
+                case '1': cw.showObservation();
+                    break;
+                case '2':
+                    String observacion = Console.ReadLine();
+                    cw.showObservation(observacion);
+                    break;
+                default:
+                    Console.WriteLine("Opcion erronea");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+
+        public static void menuIndicators(ClienteWeb cw)
+        {
+            Console.Write("1º) Mostrar todos" + "\n" +
+                "2º) Mostrar indicador " + "\n");
+            Console.Write("Seleccione una opción: ");
+            switch (Console.Read())
+            {
+                case '1': cw.showIndicator();
+                    break;
+                case '2':
+                    String indicador = Console.ReadLine();
+                    cw.showIndicator(indicador);
+                    break;
+                default:
+                    Console.WriteLine("Opcion erronea");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
