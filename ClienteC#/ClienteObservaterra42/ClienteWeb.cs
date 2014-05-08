b3c945b067f3e723d88ddb5ba4d688c153a143d3
Sergio González Polán <sergpolan@gmail.com>
2014-05-08 12:37:46 +0200

    Primera version del cliente en c#, realizando las consultas para country y observation

---
+++ ClienteC#/ClienteObservaterra42/ClienteWeb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ClienteAPI
+{
+    class ClienteWeb
+    {
+        public WebClient wc;
+
+        public ClienteWeb()
+        {
+            wc = new WebClient();
+        }
+
+        public String showCountry()
+        {
+            return wc.DownloadString("http://localhost:9000/api/country");
+        }
+
+        public String showCountry(String pais)
+        {
+            return wc.DownloadString("http://localhost:9000/api/country/" + pais);
+        }
+
+        public String showObservation()
+        {
+            return wc.DownloadString("http://localhost:9000/api/observation");
+        }
+
+        public String showObservation(String observacion)
+        {
+            return wc.DownloadString("http://localhost:9000/api/observation/" + observacion);
+        }
+
+    }
+}
